92dbfeef9d1ea64503fd6bc9cc41f4e8d3ff596b
Benjamin Schmid <B.Schmid@exxcellent.de>
2005-10-25 07:32:01 +0000

    o javadoc
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/trunk/wings2@2209 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- src/java/org/wings/event/SParentFrameListener.java
+++ src/java/org/wings/event/SParentFrameListener.java
@@ -4,18 +4,24 @@ import java.util.EventListener;
 
 /**
  * This Listener is called when a component's parent frame reference is
- * updated. It is used for registering things like headers or listeners.
+ * updated i.e. via {@link org.wings.SComponent#setParentFrame(org.wings.SFrame)}
+ * or {@link org.wings.SComponent#setParent(org.wings.SContainer)}
+ *
+ * It is used for lazy registering things like headers or listeners at the application frame as typically
+ * {@link org.wings.SComponent#getParentFrame()} will return <code>null</code> during construction time.
+
  * @author ole
  * @version $Revision$
  */
 public interface SParentFrameListener extends EventListener {
     /**
-     * called, whenever a parentFrame reference is added to the container.
+     * Called, whenever a parentFrame reference is added to the container.
+     * This i.e. happens on <code>frame.add(component)</code>.
      */
     void parentFrameAdded(SParentFrameEvent e);
 
     /**
-     * called, whenever a parentFrame reference is removed from the container.
+     * Called, whenever a parentFrame reference is removed from the container.
      */
     void parentFrameRemoved(SParentFrameEvent e);
 }
