443ae024f2c23bc6a291054e6d33d0319f317690
Armin Haaf <armin.haaf@mercatis.com>
2006-01-30 16:00:22 +0000

    o fix forgotten var in intersect
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/trunk/wings2@2442 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- src/res/org/wings/plaf/css/Menu.js
+++ src/res/org/wings/plaf/css/Menu.js
@@ -3,16 +3,20 @@
    typical js namespace clutter.
 */
 
-var wpm_menuOpen = 0; // is a menu open
-var wpm_activeMenu; // currently active menu
-var wpm_menuCalled = 0; // was the function called lately
-var wpm_lastCoord = new wpm_point(0,0); // position of currently active menu
+var wpm_menuOpen = 0;
+// is a menu open
+var wpm_activeMenu;
+// currently active menu
+var wpm_menuCalled = 0;
+// was the function called lately
+var wpm_lastCoord = new wpm_point(0, 0);
+// position of currently active menu
 /* Timeout in ms. When a menu is opened, this timeout is triggered.
    While it is active, no other menu can show which is triggered by
    an event with the same coordinates.
    This circumvents mutiple events and thus enables the use of
    cascaded menus.
-*/ 
+*/
 var wpm_timeOut = 1000;
 /* ClickDelta in px. When a click is recieved, the coordinates of the click
    are compared with the coordinates of the event that was recieved last. If
@@ -20,41 +24,42 @@ var wpm_timeOut = 1000;
    might be invalid.
    This circumvents mutiple events and thus enables the use of
    cascaded menus.
-*/ 
+*/
 var wpm_clickDelta = 0;
 var wpm_openMenus = new Array();
 
 function wpm_getEvent(e) {
-	if (window.event) return window.event;
-	else return e;
+    if (window.event) return window.event;
+    else return e;
 }
-function wpm_point(x,y) {
-	this.x = x;
-	this.y = y;
+function wpm_point(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
 function wpm_getCoordinates(e) {
-	var coord; 
-	if (wu_ie5) {
-		coord = new wpm_point(e.x, e.y);
-	} else {
-		coord = new wpm_point(e.pageX, e.pageY);
-	}
-	return coord;
+    var coord;
+    if (wu_ie5) {
+        coord = new wpm_point(e.x, e.y);
+    }
+    else {
+        coord = new wpm_point(e.pageX, e.pageY);
+    }
+    return coord;
 }
 
 function wpm_getMenuPosition(e) {
-	var e;
-	if (wu_ie5) {
-		el = e.srcElement;
-	} else {
-		el = e.target;
+    var e;
+    if (wu_ie5) {
+        el = e.srcElement;
+    }
+    else {
+        el = e.target;
         if (el.nodeType == 3) // defeat KHTML bug
             el = el.parentNode;
-	}
-	return new wpm_point(wpm_findPosX(el), wpm_findPosY(el) + el.offsetHeight);
+    }
+    return new wpm_point(wpm_findPosX(el), wpm_findPosY(el) + el.offsetHeight);
 }
-
 function wpm_findPosX(obj)
 {
     var curleft = 0;
@@ -62,15 +67,12 @@ function wpm_findPosX(obj)
     {
         while (obj.offsetParent)
         {
-                curleft += obj.offsetLeft
-                obj = obj.offsetParent;
+            curleft += obj.offsetLeft
+            obj = obj.offsetParent;
         }
-    } else if (obj.x) {
-        curleft += obj.x;
-    } else if (obj.offsetLeft) {
-        curleft += obj.offsetLeft;
     }
-    if (wu_opera) curleft += 10; // why?
+    else if (obj.x)
+        curleft += obj.x;
     return curleft;
 }
 
@@ -85,149 +87,154 @@ function wpm_findPosY(obj)
             obj = obj.offsetParent;
         }
     }
-    else if (obj.y) {
+    else if (obj.y)
         curtop += obj.y;
-    } else if (obj.offsetTop) {
-        curtop += obj.offsetTop;
-    }
-    if (wu_opera) curtop += 10; // why?
     return curtop;
 }
 
 function wpm_isValidEvent(coord) {
-	if ( (Math.abs(wpm_lastCoord.x - coord.x) <= wpm_clickDelta) && (Math.abs(wpm_lastCoord.y - coord.y) <= wpm_clickDelta) && (wpm_menuCalled == 1) ) {
-		// see timeout and clickdelta comments
-		return false;
-	} else return true;
+    if ((Math.abs(wpm_lastCoord.x - coord.x) <= wpm_clickDelta) && (Math.abs(wpm_lastCoord.y - coord.y) <= wpm_clickDelta) && (wpm_menuCalled == 1)) {
+        // see timeout and clickdelta comments
+        return false;
+    }
+    else return true;
 }
 
 function wpm_menu(e, menu) {
-	e = wpm_getEvent(e);
-	menuPos = wpm_getMenuPosition(e);
-	eventPos = wpm_getCoordinates(e);
-	if (!wpm_isValidEvent(eventPos)) return false;
-	if (e.button < 2) { // left click hopefully
-		wpm_hideActiveMenu();
-		wpm_showMenu(menu, menuPos, eventPos);
-	} 
+    e = wpm_getEvent(e);
+    menuPos = wpm_getMenuPosition(e);
+    eventPos = wpm_getCoordinates(e);
+    if (!wpm_isValidEvent(eventPos)) return false;
+    if (e.button < 2) { // left click hopefully
+        wpm_hideActiveMenu();
+        wpm_showMenu(menu, menuPos, eventPos);
+    }
 }
 
 function wpm_changeMenu(e, menu) {
-	e = wpm_getEvent(e);
-	if (wpm_menuOpen && (wpm_activeMenu != menu)) {
-		menuPos = wpm_getMenuPosition(e);
-		eventPos = wpm_getCoordinates(e);
-		wpm_hideActiveMenu();
-		wpm_showMenu(menu, menuPos, eventPos);
-	}
+    e = wpm_getEvent(e);
+    if (wpm_menuOpen && (wpm_activeMenu != menu)) {
+        menuPos = wpm_getMenuPosition(e);
+        eventPos = wpm_getCoordinates(e);
+        wpm_hideActiveMenu();
+        wpm_showMenu(menu, menuPos, eventPos);
+    }
 }
 
 function wpm_menuPopup(e, menu) {
-	e = wpm_getEvent(e);
-	var coord = wpm_getCoordinates(e);
-	if (!wpm_isValidEvent(coord)) return false;
-	if (e.type == 'mousedown') {
-		if (wu_konqueror || wu_opera || wu_safari) {
-			if (e.ctrlKey && (e.button == 2)) {
-				// ctrl right click
-				wpm_hideActiveMenu();
-				wpm_showMenu(menu, coord);
-				return false;
-			} else {
-				wpm_hideActiveMenu();
-			}
-		} else {
-			// handle other actions as contextmenu for ie && firefox and mousedown + ctrl for other browsers
-			wpm_hideActiveMenu();
-		}
-	} else if (e.type == 'contextmenu') {
-		// ie && firefox
-		if (e.ctrlKey || e.shiftKey) {
-			// only handle straight right clicks, let others pass
-			return true;
-		}
-		wpm_hideActiveMenu();
-		wpm_showMenu(menu, coord);
-		return false;
-	}
+    e = wpm_getEvent(e);
+    var coord = wpm_getCoordinates(e);
+    if (!wpm_isValidEvent(coord)) return false;
+    if (e.type == 'mousedown') {
+        if (wu_konqueror || wu_opera || wu_safari) {
+            if (e.ctrlKey && (e.button == 2)) {
+                // ctrl right click
+                wpm_hideActiveMenu();
+                wpm_showMenu(menu, coord);
+                return false;
+            }
+            else {
+                wpm_hideActiveMenu();
+            }
+        }
+        else {
+            // handle other actions as contextmenu for ie && firefox and mousedown + ctrl for other browsers
+            wpm_hideActiveMenu();
+        }
+    }
+    else if (e.type == 'contextmenu') {
+        // ie && firefox
+        if (e.ctrlKey || e.shiftKey) {
+            // only handle straight right clicks, let others pass
+            return true;
+        }
+        wpm_hideActiveMenu();
+        wpm_showMenu(menu, coord);
+        return false;
+    }
 }
 
 function wpm_hideActiveMenu() {
-	if (wpm_menuOpen == 1) {
-		document.getElementById(wpm_activeMenu).style.display = 'none';
-		wpm_openMenus = new Array();
-		wpm_toggleFormElements();
-		wpm_menuOpen = 0;
-	}
+    if (wpm_menuOpen == 1) {
+        document.getElementById(wpm_activeMenu).style.display = 'none';
+        wpm_openMenus = new Array();
+        wpm_toggleFormElements();
+        wpm_menuOpen = 0;
+    }
 }
 
 function wpm_showMenu(menuId, coord, eventCoord) {
-	elStyle = document.getElementById(menuId).style;
-	elStyle.top = coord.y + 'px';
-	elStyle.left = coord.x + 'px';
-	elStyle.display = 'block';
+    elStyle = document.getElementById(menuId).style;
+    elStyle.top = coord.y + 'px';
+    elStyle.left = coord.x + 'px';
+    elStyle.display = 'block';
+    elStyle.zIndex = 100000;
     wpm_openMenus[wpm_openMenus.length] = menuId;
-	wpm_toggleFormElements(wpm_buildBoundsArray(wpm_openMenus));
-	wpm_menuCalled = 1;
-	wpm_menuOpen = 1;
-	wpm_activeMenu = menuId;
-	setTimeout('wpm_menuCalled = 0;',wpm_timeOut);
-	if (typeof(eventCoord) == 'undefined') wpm_lastCoord = coord;
-	else wpm_lastCoord = eventCoord;
+    wpm_toggleFormElements(wpm_buildBoundsArray(wpm_openMenus));
+    wpm_menuCalled = 1;
+    wpm_menuOpen = 1;
+    wpm_activeMenu = menuId;
+    setTimeout('wpm_menuCalled = 0;', wpm_timeOut);
+    if (typeof(eventCoord) == 'undefined') wpm_lastCoord = coord;
+    else wpm_lastCoord = eventCoord;
 }
 
 function wpm_handleBodyClicks(e) {
-	e = wpm_getEvent(e);
-	coords = wpm_getCoordinates(e);
-	if (wpm_isValidEvent(coords)) {
-		wpm_hideActiveMenu();
-	}
+    e = wpm_getEvent(e);
+    coords = wpm_getCoordinates(e);
+    if (wpm_isValidEvent(coords)) {
+        wpm_hideActiveMenu();
+    }
 }
 
 function wpm_setVisible(element, visible) {
     if (visible) {
         element.style.visibility = 'visible';
-    } else {
-        element.style.visibility= 'hidden';
+    }
+    else {
+        element.style.visibility = 'hidden';
     }
 }
 
 function wpm_openMenu(id) {
-    document.getElementById(id).style.display='block';
+    document.getElementById(id).style.display = 'block';
     wpm_openMenus[wpm_openMenus.length] = id;
-	wpm_toggleFormElements(wpm_buildBoundsArray(wpm_openMenus));
+    wpm_toggleFormElements(wpm_buildBoundsArray(wpm_openMenus));
 }
 
 function wpm_closeMenu(id) {
-    document.getElementById(id).style.display='none';
-    wpm_openMenus = wpm_openMenus.slice(0,wpm_openMenus.length-1);
+    document.getElementById(id).style.display = 'none';
+    wpm_openMenus = wpm_openMenus.slice(0, wpm_openMenus.length - 1);
 }
 
 function wpm_toggleFormElements(elementBounds) {
-	if ( wu_ie5 ) {
-	    var selects = document.getElementsByTagName('select');
-	    if (!elementBounds) {
-		    for ( var i=0; i<selects.length; i++ ) {
-	        	wpm_setVisible(selects[i], true);
-		    }
-	    } else {
-		    for ( var i=0; i<selects.length; i++ ) {
-		          var select = selects[i];
-		        var selectBounds = new wpm_Bounds(select);
-		        var intersects = false;
-		        for (var b = 0; b < elementBounds.length; ++b) {
-		            var bounds = elementBounds[b];
-		            if (selectBounds.intersect(bounds)) {
-		                intersects = true;
-                      	break;
+    if (wu_ie5) {
+        var selects = document.getElementsByTagName('select');
+        if (!elementBounds) {
+            for (var i = 0; i < selects.length; i++) {
+                wpm_setVisible(selects[i], true);
+            }
+        }
+        else {
+            for (var i = 0; i < selects.length; i++) {
+                var select = selects[i];
+                var selectBounds = new wpm_Bounds(select);
+                var intersects = false;
+                for (var b = 0; b < elementBounds.length; b++) {
+                    var bounds = elementBounds[b];
+                    if (selectBounds.intersect(bounds)) {
+                        intersects = true;
+                        break;
                     }
-		        }
-		        if (intersects) {
-		        	wpm_setVisible(select, false);
-		        }
-		    } // rof
-		} // fi !elementBounds
-	}  // fi wu_ie5
+                }
+                if (intersects) {
+                    wpm_setVisible(select, false);
+                }
+            }
+            // rof
+        }
+        // fi !elementBounds
+    }  // fi wu_ie5
 }
 
 function wpm_Bounds(object) {
@@ -237,11 +244,15 @@ function wpm_Bounds(object) {
     this.height = object.offsetHeight;
 }
 
+wpm_Bounds.prototype.toString = function() {
+    return this.x + ":" + this.y + ":" + this.width + ":" + this.height;
+}
+
 wpm_Bounds.prototype.intersect = function(other) {
-    x1 = Math.max(this.x, other.x);
-    y1 = Math.max(this.y, other.y);
-    x2 = Math.min(this.x + this.width, other.x + other.width);
-    y2 = Math.min(this.y + this.height, other.y + other.height);
+    var y1 = Math.max(this.y, other.y);
+    var x1 = Math.max(this.x, other.x);
+    var x2 = Math.min(this.x + this.width, other.x + other.width);
+    var y2 = Math.min(this.y + this.height, other.y + other.height);
     if (x1 >= x2 || y1 >= y2)
         return false;
     else
@@ -249,9 +260,9 @@ wpm_Bounds.prototype.intersect = function(other) {
 }
 
 function wpm_buildBoundsArray(menuarray) {
-	var bounds = new Array(menuarray.length);
-	for (var i = 0; i < menuarray.length; i++) {
-		bounds[i] = new wpm_Bounds(document.getElementById(menuarray[i]));
-	}
-	return bounds;
+    var bounds = new Array(menuarray.length);
+    for (var i = 0; i < menuarray.length; i++) {
+        bounds[i] = new wpm_Bounds(document.getElementById(menuarray[i]));
+    }
+    return bounds;
 }
