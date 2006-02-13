48560a89da6644f06d7c31be187c484077d11c95
Benjamin Schmid <B.Schmid@exxcellent.de>
2006-02-13 13:13:08 +0000

    o use white-space:nowrap to avoid hyphen linebreaking
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/trunk/wings2@2482 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- src/java/org/wings/plaf/css/AbstractLabelCG.java
+++ src/java/org/wings/plaf/css/AbstractLabelCG.java
@@ -20,37 +20,32 @@ import java.io.IOException;
 
 public abstract class AbstractLabelCG extends AbstractComponentCG {
 
-    /** Prefer other writeText method! */
-    protected void writeText(Device device, String text) throws IOException {
+    /**
+     * Prefer other writeText method!
+     */
+    protected final void writeText(Device device, String text) throws IOException {
         writeText(device, text, false);
     }
 
-    protected void writeText(Device device, String text, boolean wordWrap) throws IOException {
+    protected final void writeText(Device device, String text, boolean wordWrap) throws IOException {
         //boolean isIE = SessionManager.getSession().getUserAgent().getBrowserType().equals(BrowserType.IE);
+
+        // white-space:nowrap seems to work in all majaor browser.
+        // Except leading and trailing spaces!
+        device.print("<span").print(wordWrap ? "" : " style=\"white-space:nowrap\"").print(">");
 
         if ((text.length() > 5) && (text.startsWith("<html>"))) {
             Utils.writeRaw(device, text.substring(6));
-        } else /*if (isIE) */{
-            //// IE doesn't handle whitespace:pre correctly, so we do it hardcore by quoting spaces
-            //// or not (depending on wordwrap)
+        } else {
+            // NOTE: Quoting of spaces would be only necessary for trailing and leading spaces !!!
+            // TODO: Quote only trailing/leading spaces
             Utils.quote(device, text, true, !wordWrap, false);
-        }   // Handle IE and Gecko same: Gecko trimmed labels whitespaces with the white-space:pre
-            // approach
-        /*  else {
-            // Other browser handle the CSS property 'white-space' correctly.
-            // The default is set as in swing: don't wrap:
-            if (!wordWrap)
-                Utils.quote(device, text, false, false, false);
-            else {
-                // non-default case: overwrite css property, set back to wrap
-                device.print("<span style=\"white-space: normal;\">");
-                Utils.quote(device, text, true, false, false);
-                device.print("</span>");
-            }
-        } */
+        }
+
+        device.print("</span>");
     }
 
-    protected void writeIcon(Device device, SIcon icon) throws IOException {
+    protected final void writeIcon(Device device, SIcon icon) throws IOException {
         device.print("<img");
         Utils.optAttribute(device, "src", icon.getURL());
         Utils.optAttribute(device, "width", icon.getIconWidth());
