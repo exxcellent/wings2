71f61064c64cafa0c3e22fee29024c8218212c11
Benjamin Schmid <B.Schmid@exxcellent.de>
2007-09-25 23:59:16 +0000

    o What a dumb "hello wings". Revert commit #2661
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/wings2/trunk@3514 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- doc/tutorial/hellowings/src/HelloWingS.java
+++ doc/tutorial/hellowings/src/HelloWingS.java
@@ -1,49 +1,4 @@
-import org.wings.SForm;
-import org.wings.SFrame;
-import org.wings.SGridLayout;
-import org.wings.SList;
-import org.wings.SButton;
-
-import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
-public class HelloWingS {
-    public HelloWingS() {
-        SGridLayout gridLayout = new SGridLayout(1);
-        SForm panel = new SForm(gridLayout);
-        gridLayout.setVgap(10);
-
-        final DefaultListModel model = new DefaultListModel();
-        for ( int i = 1; i <= 10; i++ ) {
-            model.addElement( new Integer(i) );
-        }
-
-        final SList list = new SList( model );
-        list.setSelectionMode( SList.SINGLE_SELECTION );
-        list.addListSelectionListener( new ListSelectionListener() {
-            public void valueChanged( ListSelectionEvent event ) {
-                System.out.println( "event value is adjusting = " + event.getValueIsAdjusting() );
-                int firstIndex = event.getFirstIndex();
-                System.out.println( "first index = " + firstIndex );
-                int lastIndex = event.getLastIndex();
-                System.out.println( "last index = " + lastIndex );
-                Object selected = model.get( lastIndex );
-                System.out.println( selected );
-            }
-        });
-
-        panel.add( list );
-        panel.add(new SButton("submit"));
-
-        SFrame rootFrame = new SFrame();
-        rootFrame.getContentPane().add(panel);
-        rootFrame.setVisible(true);
-    }
-}
-
-
-/*import org.wings.*;
+import org.wings.*;
 import java.util.*;
 import java.awt.event.*;
 
@@ -83,4 +38,4 @@ public class HelloWingS {
         rootFrame.getContentPane().add(panel);
         rootFrame.setVisible(true);
     }
-}*/
+}
